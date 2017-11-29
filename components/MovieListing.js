92f5219a3169758215b8919a671089c0065295dd
Colin Jaffe <balloonasaurus@gmail.com>
2017-11-29 18:08:21 -0500

    simplified code in MovieListing component

--- components/MovieListing.js
+++ components/MovieListing.js
@@ -21,7 +21,9 @@ const styles = StyleSheet.create({
   posterContainer: {
     flex: 1,
     height: 180,
-    width: 90
+    width: 90,
+    borderWidth: 5,
+    
   },
   leftSide: {
     flex: 1,
@@ -47,41 +49,31 @@ const styles = StyleSheet.create({
 const RED = 'red';
 const GREEN = 'green';
 
-const MovieListing = ({movie, navigation}) => {
-  const {navigate} = navigation;
-  const {title, year, cloudinaryName} = movie;
-  let {posterUri} = movie;
-  posterUri = posterUri ? posterUri : 'o';
-  let button = (
-    <Button
-      onPress={() => navigate('MoviePageScreen', {movie})}
+const MovieListing = ({movie, navigation}) => (
+  <View style={styles.row}>
+    <View style={styles.posterContainer}>
+      {movie.posterUri
+      ? <Image source={{uri: movie.posterUri}} style={styles.poster} />
+      : null}
+    </View>
+    <View style={styles.leftSide}>
+      <Text style={styles.emphasis}>{`${movie.title} - ${movie.year}`}</Text>
+    </View>
+    <View style={styles.rightSide}>
+      {movie.cloudinaryName
+      ? <Button
+      onPress={() => navigation.navigate('MoviePageScreen', {movie})}
       title={`Play`}
       color={GREEN}
     />
-  );
-  if (!cloudinaryName) {
-    button = (
-      <Button
-        onPress={() => navigate('MoviePageScreen', {movie})}
-        title={`Help us find it!`}
-        color={RED}
-      />
-    );
-  }
-  return (
-    <View style={styles.row}>
-      <View style={styles.posterContainer}>
-        <Image source={{uri: posterUri}} style={styles.poster} />
-      </View>
-      <View style={styles.leftSide}>
-        <Text style={styles.emphasis}>{`${title} - ${year}`}</Text>
-      </View>
-      <View style={styles.rightSide}>
-        {button}
-      </View>
+      : <Button
+      onPress={() => navigation.navigate('MoviePageScreen', {movie})}
+      title={`Help us find it!`}
+      color={RED}
+    />}
     </View>
-  );
-};
+  </View>
+);
 
 export default MovieListing;
 
